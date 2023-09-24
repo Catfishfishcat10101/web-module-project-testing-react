c5f483a3c7dfda4de1bf8052946b33dafe28ed3b
William Johnson <catfishfishcat10101@gmail.com>
2023-09-24 04:34:03 -0500

    added episode.test

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,26 +3,43 @@ import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Episode from "./../Episode";
 
-test("renders without error", () => {});
 
-test("renders the summary test passed as prop", () => {});
 
-test("renders default image when image is not defined", () => {});
+test("renders without error", () => {
+  // Render the component
+  render(<Episode />);
+  // Assert that the component is present in the document
+  const episode = screen.getByTestId('episode');
+  expect(episode).toBeInTheDocument();
+});
 
-// ----- EXAMPLE EPISODE TEST OBJECT -----
-// const exampleEpisodeData = {
-//   airdate: "2016-07-15",
-//   airstamp: "2016-07-15T12:00:00+00:00",
-//   airtime: "",
-//   id: 553946,
-//   image: "https://static.tvmaze.com/uploads/images/medium_landscape/342/855786.jpg",
-//   name: "Chapter One: The Vanishing of Will Byers",
-//   number: 1,
-//   rating: { average: 8.2 },
-//   runtime: 49,
-//   season: 1,
-//   summary:
-//     "A young boy mysteriously disappears, and his panicked mother demands that the police find him. Meanwhile, the boy's friends conduct their own search, and meet a mysterious girl in the forest.",
-//   type: "regular",
-//   url: "https://www.tvmaze.com/episodes/553946/stranger-things-1x01-chapter-one-the-vanishing-of-will-byers",
-// };
+test("renders the summary test passed as prop", () => {
+  const exampleEpisode = {
+    id: 1,
+    name: 'Example Episode',
+    season: 1,
+    number: 1,
+    summary: 'This is an example summary.',
+    runtime: 45,
+  };
+  render(<Episode episode={exampleEpisode} />);
+
+  const summary = screen.getByText('This is an example summary');
+  expect(summary).toBeInTheDocument();
+});
+
+test("renders default image when image is not defined", () => {
+    const exampleEpisode = {
+      id: 1,
+      name: "Example Episode",
+      season: 1,
+      summary: "This is a example summary",
+      runtime: 45,
+    };
+    render(<Episode episode={exampleEpisode} />);
+    const defaultImage = screen.getByAltText('./stranger_things.png');
+    expect(defaultImage).toBeInTheDocument();
+
+    fireEvent.click(defaultImage);
+    expect(defaultImage).toHaveClass('active');
+  });
